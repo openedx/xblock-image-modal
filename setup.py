aa2fb9f8c0d65a5187eab378262d1683355345a3
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-11-24 16:35:04 +0500

    build: Rename the package name. `xblock-image-modal` already exists on pypi.
    Adding functionality for publishing on pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 
 from setuptools import setup
 
-version = '2.1.1'
+version = '3.0.0'
 description = __doc__.strip().split('\n')[0]
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.rst')) as file_in:
@@ -81,7 +81,7 @@ def is_requirement(line):
 
 
 setup(
-    name='xblock-image-modal',
+    name='edx-xblock-image-modal',
     version=version,
     description=description,
     long_description=long_description,
