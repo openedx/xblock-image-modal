5f0412bb6317304e78d3129a04a3b0456df665f1
Feanil Patel <feanil@axim.org>
2024-09-10 11:01:01 -0400

    build: Drop building with python 3.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 
 from setuptools import setup
 
-version = '3.3.0'
+version = '4.0.0'
 description = __doc__.strip().split('\n')[0]
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.rst')) as file_in:
@@ -119,8 +119,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: JavaScript',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Education',
         'Topic :: Internet :: WWW/HTTP',
         'Framework :: Django',
